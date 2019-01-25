7b01a2035c034e36f10f812be2f4eafd06f4f4a2
CamilaAoulad <camila.aoulad@gmail.com>
2019-01-25 12:04:58 +0100

    first commit

---
+++ Ecole/src/fr/adaming/ecole/humain/Prof.java
@@ -0,0 +1,43 @@
+package fr.adaming.ecole.humain;
+
+public class Prof extends Humain{
+
+	String diplome; 
+	int salaire; 
+	
+	public Prof() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Prof(int salaire) {
+		super();
+		this.salaire = salaire;
+	}
+	
+
+	public Prof(String diplome) {
+		super();
+		this.diplome = diplome;
+	}
+
+	//Constructor using field 
+	public Prof(String diplome, int salaire) {
+		super();
+		this.diplome = diplome;
+		this.salaire = salaire;
+	}
+
+	public void enseigner() {
+		System.out.println("J enseigne .....");
+	}
+
+	@Override
+	public String toString() {
+		return "Prof [diplome=" + diplome + ", salaire=" + salaire + ", nom=" + nom + ", prenom=" + prenom + ", age="
+				+ age + ", adresse=" + adresse + ", sex=" + sex + "]";
+	}
+	
+	
+}
+
